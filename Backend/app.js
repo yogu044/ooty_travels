dbe86567539c43d2b43f2e0d39609c41097e3873
YOGESH D <yogu044@users.noreply.github.com>
2025-11-03 14:14:58 +0530

    Clean backend dependencies for Vercel

--- Backend/app.js
+++ Backend/app.js
@@ -3,17 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const connectDB=require('./config/mongo')
-const cors=require('cors')
+const connectDB = require('./config/mongo');
+const cors = require('cors');
 
-connectDB()
+connectDB();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const detailRouter=require('./routes/Details')
-const contactRouter=require('./routes/Conatcts')
+const detailRouter = require('./routes/Details');
+const contactRouter = require('./routes/Conatcts');
 
 var app = express();
+
+// ✅ Add CORS configuration here
+app.use(cors({
+  origin: ["https://ootytours.netlify.app"], // your Netlify frontend
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,25 +31,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/details',detailRouter);
-app.use('/contact',contactRouter);
+app.use('/details', detailRouter);
+app.use('/contact', contactRouter);
 
-
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-
+// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
